24738d293f960d5b688ea8cad74edebbf8f19cb0
puttipong95 <pgmoney1955@gmail.com>
2022-06-14 18:44:27 +0700

    Add Index Banner and Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,31 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Row from '../components/Row'
+import { Movie } from '../typings'
 import requests from '../utils/requests'
 
-const Home = ({ netflixOriginals }) => {
-  console.log(netflixOriginals);
-  
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({ 
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries
+ }: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -14,15 +34,17 @@ const Home = ({ netflixOriginals }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <main>
-        <Banner/>
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+        <Banner netflixOriginals={netflixOriginals}/>
         <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Components*/}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
     </div>
