8bd565b9a7378f6ff17cc9aaef7693a893522c54
puttipong95 <pgmoney1955@gmail.com>
2022-07-02 20:47:34 +0700

    Modal - Fetch data movie

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MuiModal from '@mui/material/Modal'
 import { useRecoilState, useRecoilValue } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
+import { modalState, movieState } from '../atoms/modalAtom'
+import { XIcon } from '@heroicons/react/outline'
 
 function Modal() {
     const [showModal, setShowModal] = useRecoilState(modalState)
+    const [movie, setMovie] = useRecoilState(movieState)
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+      if(!movie) return
+
+      async function fetchMovie() {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/${
+            movie?.media_type === 'tv' ? 'tv' : 'movie'
+          }/${movie?.id}?api_key=${
+            process.env.NEXT_PUBLIC_API_KEY
+          }&language=en-US&append_to_response=videos`
+        ).then((response) => response.json())
+        .catch(err => console.log(err.message))
+        
+        setData(data)
+      }
+
+      fetchMovie()
+    }, [movie])
+
+    console.log(data);
 
     const handleClose = () => {
         setShowModal(false)
@@ -13,7 +37,16 @@ function Modal() {
   return (
     <MuiModal open={showModal} onClose={handleClose}>
         <>
-            Modal
+            <button 
+              className='modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818 hover:bg-[#181818]'
+              onClick={handleClose} 
+            >
+              <XIcon className='h-6 w-6' />
+            </button>
+
+            <div>
+
+            </div>
         </>
     </MuiModal>
   )
